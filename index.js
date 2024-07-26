1b0d576ae48142cb4f499d481bf7456169a63335
rehanmm <21bcs049@nith.ac.in>
2024-07-26 14:28:08 +0530

    added source file

---
+++ index.js
@@ -0,0 +1,11 @@
+const app = require('./src/server');
+app.listen(3000, function (err) {
+    if(err){
+        console.log("error while starting server");
+        console.log(err);
+        console.log(err.stack);
+    }
+    else{
+        console.log("server has been started at port "+3000);
+    }
+})
--- src/server.js
+++ src/server.js
@@ -19,17 +19,7 @@ process.on('SIGTERM', exitHandler(0,'SIGTERM'))
 process.on('SIGINT',exitHandler(0, 'SIGTINT'))
 process.on('unhandledRejection',exitHandler(0,'unhandledRejection'))    
 
-
-app.listen(process.env.PORT, function (err) {
-    if(err){
-        console.log("error while starting server");
-        console.log(err.stack||err);
-
-    }
-    else{
-        console.log("server has been started at port "+process.env.PORT);
-    }
-})
+module.exports=app;
 
 
 
